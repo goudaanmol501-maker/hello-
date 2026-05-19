0a289fa1ee08645df57cd64b3c5392a1b7dbbbe1
Anmol SG <goudaanmol501@gmail.com>
2026-05-19 23:40:04 +0530

    Add addition table function and call it

--- hello-world.ts
+++ hello-world.ts
@@ -7,10 +7,15 @@ function printTable(num: number): void {
     console.log(`${num} × ${i} = ${num * i}`);
   }
 }
-
+function addTable(num: number):void {
+  console.log('\n=== Addition of ${num} ===');
+  for (let i=0;i<=10; i++){
+    console.log('${num} + ${i} =${num+i}');
+  }
+}
 // Calculate 4 multiplication tables
 printTable(2);
 printTable(3);
 printTable(5);
-
+addTable(4);s
 printTable(10);
